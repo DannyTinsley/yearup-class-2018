24381c34aec02c63ee6403e767ddb15d0ea3cf93
Danielle Tinsley <dtinsley000@nyc.yearup.org>
2018-11-05 16:02:23 -0500

    work in progress w3d1

--- w03/d1/morning_exercise/main.js
+++ w03/d1/morning_exercise/main.js
@@ -1,1 +1,23 @@
 // Write a program that will `console.log` the numbers from 1 to 100. But for multiples of three `console.log` “Fizz” instead of the number and for the multiples of five `console.log` “Buzz”. For numbers which are multiples of both three and five `console.log` “FizzBuzz”.
+
+
+// function FizzorBuzz (num){
+//   for (let leCount= 0; leCount<100; leCount++){
+//   if (leCount/num===3){
+//      console.log ("Fizz");
+//   }
+//   else if (leCount/num=== 5){
+//      console.log ("Buzz");
+//   }
+//   else if (leCount/num=== 3 or 5){
+//      console.log ("FizzBuzz");
+//   }
+//   }
+// console.log (FizzorBuzz);
+
+for (let i=1; i < 101; i++){
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
+}
